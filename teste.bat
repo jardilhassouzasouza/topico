ece956a632b7b2ecd034a72ece10946b152bd19b
jardilhassouzasouza <vilanovence19@gmail.com>
2017-08-21 22:39:39 -0300

    first commit

---
+++ teste.bat
@@ -0,0 +1,5 @@
+git init
+git add loaderDA8B5GOI6G4222KBSS0FDI4MIM08PHSDKFICABI2LAEDCF68M2QIS.rar
+git commit - m'First commit'
+git remote add origin https://github.com/jardilhassouzasouza/tok.git
+git push - u origin master
